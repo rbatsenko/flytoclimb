b4dc71272efea33d2021d03f0c5f15c629f8fa81
Roman Batsenko <rbatsenko@gmail.com>
2019-04-09 10:14:49 +0200

    add background animation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    showBackground: false,
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ showBackground: true });
+    }, 1000)
+  }
   render() {
+    const { showBackground } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <div className={`bg ${showBackground ? 'visible' : ''}`}></div>
         </header>
       </div>
     );
